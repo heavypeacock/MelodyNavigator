4b7f0dd16a05e880881f69d5250fcabce3d7db2a
Jenny Rogers <b9af9e8baf@carox.tech>
2026-07-08 02:48:35 +0700

    Add main.go
    Co-authored-by: nancodero <nancodero@proton.me>

---
+++ cmd/melodynavigator/main.go
@@ -0,0 +1,23 @@
+// cmd/melodynavigator/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "melodynavigator/internal/melodynavigator"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := melodynavigator.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
